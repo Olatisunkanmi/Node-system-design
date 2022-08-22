8d37556712d3d3376349b0a8863b2f84b3c9711c
Igbasan Olasunkanmi <86102079+Olatisunkanmi@users.noreply.github.com>
2022-08-22 18:48:55 +0100

    First commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./index');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
 	console.log(`Node is listening on Port ${port}`);
 });
